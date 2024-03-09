490a2a9a03671d2e30d01f0a6523b4db1f4c4520
sanjakv <162290716+sanjakv@users.noreply.github.com>
2024-03-09 20:08:17 +0600

    tfuyggyg

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,3 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-System.Console.WriteLine("Helooo Sharpister!!!!");
+
+using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Helooo Sharpister!!!!");
+
+            string str = Console.ReadLine();
+            string rule = "все тупые";
+            if (rule == str)
+            {
+                Console.WriteLine("поздровляю вы прошли проверку");
+            }
+            else
+            {
+                Console.WriteLine("неверно");
+
+            }          
+        } 
+    }
+ }
